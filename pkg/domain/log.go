41da0e2cb308e55fc0d3b7dfc83e28031032cd47
zhashkevych <mac@mksgoods-MacBook-Pro.local>
2021-09-27 13:28:27 +0300

    added actions & entities constants

--- pkg/domain/log.go
+++ pkg/domain/log.go
@@ -1,10 +1,59 @@
 package audit
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+const (
+	ENTITY_USER = "USER"
+	ENTITY_BOOK = "BOOK"
+
+	ACTION_CREATE   = "CREATE"
+	ACTION_UPDATE   = "UPDATE"
+	ACTION_GET      = "GET"
+	ACTION_DELETE   = "DELETE"
+	ACTION_REGISTER = "REGISTER"
+	ACTION_LOGIN    = "LOGIN"
+)
+
+var (
+	entities = map[string]LogRequest_Entities{
+		ENTITY_USER: LogRequest_USER,
+		ENTITY_BOOK: LogRequest_BOOK,
+	}
+
+	actions = map[string]LogRequest_Actions{
+		ACTION_CREATE:   LogRequest_CREATE,
+		ACTION_UPDATE:   LogRequest_UPDATE,
+		ACTION_GET:      LogRequest_GET,
+		ACTION_DELETE:   LogRequest_DELETE,
+		ACTION_REGISTER: LogRequest_REGISTER,
+		ACTION_LOGIN:    LogRequest_LOGIN,
+	}
+)
 
 type LogItem struct {
 	Entity    string    `bson:"entity"`
 	Action    string    `bson:"action"`
 	EntityID  int64     `bson:"entity_id"`
 	Timestamp time.Time `bson:"timestamp"`
+}
+
+func ToPbEntity(entity string) (LogRequest_Entities, error) {
+	val, ex := entities[entity]
+	if !ex {
+		return 0, errors.New("invalid entity")
+	}
+
+	return val, nil
+}
+
+func ToPbAction(action string) (LogRequest_Actions, error) {
+	val, ex := actions[action]
+	if !ex {
+		return 0, errors.New("invalid action")
+	}
+
+	return val, nil
 }
